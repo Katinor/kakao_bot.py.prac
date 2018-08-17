8130bcb9e0c9bd7d31179d55e13f5d60b4469673
Katinor <katinor@4ears.net>
2018-08-17 19:01:40 +0900

    update api

--- main.py
+++ main.py
@@ -8,15 +8,19 @@ import kakaobot
 app = kakaobot.Client(port = 7900)
 
 @app.add_command
-def 사잽아():
+def 사잽아(user_key):
 	return kakaobot.Message(text = "안녕!")
 
+@app.add_alias_command(["안녕","반가워"])
+def greeting(user_key):
+	return kakaobot.Message(text = "나도 반가워!")
+
 @app.add_prefix_command
-def 따라해(content):
+def 따라해(user_key, content):
 	return kakaobot.Message(text = content)
 
 @app.set_extra
-def extra_func(content):
+def extra_func(user_key, content):
 	return kakaobot.Message(text = content)
 
 app.run()
